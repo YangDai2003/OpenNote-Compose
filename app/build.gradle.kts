9fd25ce8b45dbb2044bae89325fa8b103e5ddd51
Yang Dai <dy15800837435@gmail.com>
2024-03-27 22:10:05 +0100

    update app logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.yangdai.opennote"
         minSdk = 29
         targetSdk = 34
-        versionCode = 102
-        versionName = "1.0.2"
+        versionCode = 103
+        versionName = "1.0.3"
         resourceConfigurations += listOf("en", "zh")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
