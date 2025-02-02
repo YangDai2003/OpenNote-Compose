795adb42d5dc3d0522d815144915ea720034aa86
Yang Dai <dy15800837435@gmail.com>
2025-02-02 14:47:49 +0100

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,22 +8,3 @@ plugins {
     alias(libs.plugins.room) apply false
     alias(libs.plugins.serialization) apply false
 }
-
-//subprojects {
-//    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-//        kotlinOptions {
-//            if (project.findProperty("composeCompilerReports") == "true") {
-//                freeCompilerArgs += listOf(
-//                    "-P",
-//                    "plugin:androidx.compose.compiler.plugins.kotlin:reportsDestination=${project.buildDir.absolutePath}/compose_compiler"
-//                )
-//            }
-//            if (project.findProperty("composeCompilerMetrics") == "true") {
-//                freeCompilerArgs += listOf(
-//                    "-P",
-//                    "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=${project.buildDir.absolutePath}/compose_compiler"
-//                )
-//            }
-//        }
-//    }
-//}
